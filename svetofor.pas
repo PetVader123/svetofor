a8e21627ed5fc7289f77e773517e9164cb92bb8d
grilled_cheese <peter1280b@gmail.com>
2024-02-25 19:43:41 +0300

    svetofor

---
+++ svetofor.pas
@@ -0,0 +1,16 @@
+﻿program svetofor;
+var
+  t,c,x:integer;
+begin
+  c:=5;
+  read(t);
+  x:=t mod c;
+  if x mod 3 = 0 then
+  
+    writeln('red')
+  
+   else
+   
+     writeln('green');
+   
+end.
